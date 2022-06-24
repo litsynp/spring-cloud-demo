f91c310b8ea0cd6ec3f6f62140924f1525219000
litsynp <nocte55is@gmail.com>
2022-06-24 22:09:08 +0900

    feat: replace API call with openfeign

--- post-api/src/main/java/com/litsynp/postapi/PostApiApplication.java
+++ post-api/src/main/java/com/litsynp/postapi/PostApiApplication.java
@@ -2,12 +2,13 @@ package com.litsynp.postapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PostApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PostApiApplication.class, args);
     }
-
 }
--- post-api/src/main/java/com/litsynp/postapi/service/MemberRemoteService.java
+++ post-api/src/main/java/com/litsynp/postapi/service/MemberRemoteService.java
@@ -1,6 +1,12 @@
 package com.litsynp.postapi.service;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(value = "member", url = "http://localhost:8081/")
 public interface MemberRemoteService {
 
-    String getMemberInfo(Long memberId);
+    @GetMapping("/members/{memberId}")
+    String getMemberInfo(@PathVariable Long memberId);
 }
